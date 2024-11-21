f3236d526c1e3e0b4348edc814df8d530f687183
FaresAkmjoud <faresalmjoud@gmail.com>
2024-11-21 19:10:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -422,4 +422,4 @@ client.on('interactionCreate', async interaction => {
 });
 
 
-client.login("MTMwOTE4NDY2NTU3OTU1Njk2NA.Gkf4aG.MO0Y1bV_A-FQdMjBWn2mZvAlF7aiS3KfHbqgq8");
+client.login("MTMwOTE4NDY2NTU3OTU1Njk2NA.GwthkG.oOPDm29-FRrwInpWpB51yqy37qmX6EaCqnvqgU");
